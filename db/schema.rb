e7e83f27ab191097761e420dfbcb469dbf4243fe
Gessivam Junior <gessivamjunior@gmail.com>
2025-08-26 23:26:14 -0300

    feat: add Restaurant reference to Menu and declare association in both models

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_08_27_021536) do
+ActiveRecord::Schema[8.0].define(version: 2025_08_27_022309) do
   create_table "menu_items", force: :cascade do |t|
     t.string "name", null: false
     t.decimal "price", precision: 6, scale: 2, null: false
@@ -35,6 +35,8 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_021536) do
     t.integer "ends_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "restaurant_id", null: false
+    t.index ["restaurant_id"], name: "index_menus_on_restaurant_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -55,4 +57,5 @@ ActiveRecord::Schema[8.0].define(version: 2025_08_27_021536) do
   end
 
   add_foreign_key "menu_items", "menus"
+  add_foreign_key "menus", "restaurants"
 end
