f81ccddd61e8bd4c25a575526fb653350202c3c9
Gessivam Junior <gessivamjunior@gmail.com>
2025-08-27 12:53:21 -0300

    test: update the MenuItemController request spec and menu_items factory

--- spec/factories/menu_items.rb
+++ spec/factories/menu_items.rb
@@ -13,8 +13,12 @@ FactoryBot.define do
       available { false }
     end
 
+    trait :cheap do
+      price { 9.99 }
+    end
+
     trait :expensive do
-      price { 49.99 }
+      price { 100.99 }
     end
 
     trait :main_course do
@@ -31,6 +35,40 @@ FactoryBot.define do
 
     trait :without_image do
       image_url { nil }
+    end
+
+    trait :with_menu do
+      after(:create) do |menu_item, evaluator|
+        menu = evaluator.menu || create(:menu)
+        menu.menu_items << menu_item
+      end
+    end
+
+    transient do
+      menu { nil }
+    end
+
+    trait :beverage do
+      category { "beverage" }
+      price { 4.99 }
+      prep_time_minutes { 5 }
+    end
+
+    trait :side_dish do
+      category { "side" }
+      price { 6.99 }
+      prep_time_minutes { 8 }
+    end
+
+    trait :complete do
+      name { "Premium Dish" }
+      description { "A complete dish with all details" }
+      category { "main_course" }
+      price { 28.99 }
+      currency { "USD" }
+      available { true }
+      prep_time_minutes { 30 }
+      image_url { "https://example.com/premium-dish.jpg" }
     end
   end
 end
