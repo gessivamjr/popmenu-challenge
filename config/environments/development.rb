fa3f6a7f099c938fc7c885f62d96ecc810e44108
Gessivam Junior <gessivamjunior@gmail.com>
2025-08-27 18:43:45 -0300

    chore: setup SolidQueue database and configurations to enable processing jobs

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -44,6 +44,12 @@ Rails.application.configure do
   # Highlight code that enqueued background job in logs.
   config.active_job.verbose_enqueue_logs = true
 
+  # Use solid_queue for background jobs.
+  config.active_job.queue_adapter = :solid_queue
+
+  # Write queue jobs to the queue database.
+  config.solid_queue.connects_to = { database: { writing: :import_restaurants_queue } }
+
   # Raises error for missing translations.
   # config.i18n.raise_on_missing_translations = true
 
