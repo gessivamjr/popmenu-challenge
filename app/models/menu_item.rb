1674c7738633f6fcf0c98179a8526b0bd3384987
Gessivam Junior <gessivamjunior@gmail.com>
2025-08-26 22:16:29 -0300

    feat: add sample scopes to Menu and MenuItem

--- app/models/menu_item.rb
+++ app/models/menu_item.rb
@@ -4,4 +4,9 @@ class MenuItem < ApplicationRecord
   validates :name, presence: true
   validates :price, presence: true, numericality: { greater_than_or_equal_to: 0 }
   validates :currency, presence: true
+
+  scope :available, ->(available) { where(available: available) }
+  scope :by_category, ->(category) { where(category: category) }
+  scope :cheap, -> { where("price < ?", 10) }
+  scope :expensive, -> { where("price > ?", 100) }
 end
